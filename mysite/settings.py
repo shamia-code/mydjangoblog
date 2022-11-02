55d65825d0f6cedd4397a5899ff7b6dd5cb0382e
shamia-code <shamienamagembe@gmail.com>
2022-11-02 14:03:59 +0300

    problem solved in urls.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,4 +127,4 @@ STATIC_ROOT = BASE_DIR/'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = Path(BASE_DIR / 'media')
+MEDIA_ROOT = Path(BASE_DIR /'media')
